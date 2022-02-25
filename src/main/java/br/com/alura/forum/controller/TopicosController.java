9660d460a756596e6f1358d6cd0d66c75d450e71
joão victor <joaovictorf01@gmail.com>
2022-02-25 15:47:36 -0300

    Pequenas mudanças adicionando uma dependencia  dev

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,14 +5,18 @@ import br.com.alura.forum.modelo.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-@Controller public class TopicosController {
+
+@RestController
+public class TopicosController {
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+    
+    public List<Topico> lista() {
+        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 
         return Arrays.asList(topico, topico, topico);
-    }}
+    }
+}
